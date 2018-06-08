f266c7f1ca201bbbb0ff5c038e6f5b2fca3b3955
chris <chris@thinkdataworks.com>
2018-06-08 13:09:42 -0400

    feature/5: Added result set iterator tests Fixes #5

--- src/test/java/namara/client/ClientTest.java
+++ src/test/java/namara/client/ClientTest.java
@@ -4,70 +4,138 @@ import namara.client.exception.AuthorizationException;
 import namara.client.exception.ConnectionException;
 import namara.client.exception.NamaraException;
 import namara.client.exception.QueryException;
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
+
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class ClientTest {
-    /*
-     * NOTE: update this with necessary API keys and URLs
-     */
-    String API_KEY = "67902e8fe69efa4d82348de8bf5df077552cc66221f15c7593fb381113f69147";
-    String NAMARA_HOST = "https://namara-api-chris.ngrok.io/";
-    String DATA_SET_ID = "e276a9a8-d06f-49a0-bd69-482920006b53";
+    String NAMARA_HOST = "https://example.namara.io/";
+    String API_KEY = "XXXXX";
+    String DATA_SET_ID = "733934b4-5434-43a6-a487-cdf8091b3a493";
     String DATA_SET_VERSION = "en-0";
 
+
     @Test
-    public void testTestConnection() throws NamaraException {
+    public void testTestConnection() throws NamaraException, IOException {
+        MockWebServer server = new MockWebServer();
+        server.enqueue(new MockResponse().setResponseCode(200).setBody("{\"status\":\"true\"}"));
+
+        HttpUrl baseUrl = server.url(Client.Endpoints.TESTING_ENDPOINT);
+
         Client client = new Client(NAMARA_HOST, API_KEY);
-        assertEquals(client.testConnection(), true);
+        assertEquals(client.testConnection(baseUrl), true);
+
+        server.shutdown();
     }
 
-    @Test(expected = ConnectionException.class)
-    public void testTestConnectionMalformedURL() throws NamaraException {
-        Client client = new Client("somemalformedUrl//://", API_KEY);
-        client.testConnection();
-    }
 
     @Test(expected = ConnectionException.class)
-    public void testTestConnectionHostNotFound() throws NamaraException {
-        Client client = new Client("https://example.namara.io", API_KEY);
-        client.testConnection();
+    public void testTestConnectionHostNotFound() throws NamaraException, IOException {
+        MockWebServer server = new MockWebServer();
+        server.enqueue(new MockResponse().setResponseCode(0).setBody(""));
+
+        try {
+            HttpUrl baseUrl = server.url(Client.Endpoints.TESTING_ENDPOINT);
+
+            Client client = new Client(NAMARA_HOST, API_KEY);
+            client.testConnection(baseUrl);
+
+            server.shutdown();
+        } catch(NamaraException e) { // catch and rethrow to shut down mock server
+            server.shutdown();
+            throw e;
+        }
     }
 
     @Test(expected = AuthorizationException.class)
-    public void testTestConnectionUserNotFound() throws NamaraException {
-        Client client = new Client(NAMARA_HOST, "XXXX");
-        client.testConnection();
+    public void testTestConnectionUserNotFound() throws NamaraException, IOException {
+        MockWebServer server = new MockWebServer();
+        server.enqueue(new MockResponse().setResponseCode(422).setBody("{\"status\":\"false\"}"));
+
+        try {
+            HttpUrl baseUrl = server.url(Client.Endpoints.TESTING_ENDPOINT);
+
+            Client client = new Client(NAMARA_HOST, API_KEY);
+            client.testConnection(baseUrl);
+
+            server.shutdown();
+        } catch(NamaraException e) { // catch and rethrow to shut down mock server
+            server.shutdown();
+            throw e;
+        }
     }
 
     @Test
-    public void testGetNamaraHost() {
-        Client client = new Client(NAMARA_HOST, API_KEY);
-        assertEquals(client.getNamaraHost(), "namara-api-chris.ngrok.io");
-    }
+    public void testQuerySuccess() throws NamaraException, IOException {
+        MockWebServer server = new MockWebServer();
+        JSONArray resultsCollection = new JSONArray()
+                .put(new JSONObject().put("c0", "xx").put("c1", "yy"))
+                .put(new JSONObject().put("c0", "zz").put("c1", "tt"))
+                .put(new JSONObject().put("c0", "hh").put("c1", "aa"));
 
-    @Test
-    public void testQuerySuccess() throws NamaraException {
-        Client client = new Client(NAMARA_HOST, API_KEY);
-        String queryString = "SELECT * FROM " + DATA_SET_ID + "/" + DATA_SET_VERSION;
-        JSONObject response = client.query(queryString);
-        assertEquals(250, response.getJSONArray("results").length());
+        server.enqueue(new MockResponse().setResponseCode(200).setBody(
+           new JSONObject().put("results", resultsCollection).toString()
+        ));
+
+        try {
+            HttpUrl baseUrl = server.url(Client.Endpoints.QUERY_ENDPOINT);
+
+            Client client = new Client(NAMARA_HOST, API_KEY);
+            String queryString = "SELECT * FROM " + DATA_SET_ID + "/" + DATA_SET_VERSION;
+
+            JSONObject response = client.query(baseUrl, queryString);
+            assertEquals(3, ((JSONArray) response.get("results")).length());
+
+            server.shutdown();
+        } catch(NamaraException e) { // catch and rethrow to shut down mock server
+            server.shutdown();
+            throw e;
+        }
     }
 
     @Test(expected = QueryException.class)
-    public void testQueryBadQuery() throws NamaraException {
-        Client client = new Client(NAMARA_HOST, API_KEY);
-        String queryString = "SELECT * FROM A BIG DATA SET";
-        client.query(queryString);
+    public void testQueryBadQuery() throws NamaraException, IOException {
+        MockWebServer server = new MockWebServer();
+        server.enqueue(new MockResponse().setResponseCode(422).setBody("{\"error\": \"Your query is bad and you should feel bad\""));
+
+        try {
+            HttpUrl baseUrl = server.url(Client.Endpoints.QUERY_ENDPOINT);
+
+            Client client = new Client(NAMARA_HOST, API_KEY);
+            String queryString = "SELECT * FROM A BIG DATA SET";
+
+            JSONObject response = client.query(baseUrl, queryString);
+        } catch(NamaraException e) {
+            server.shutdown();
+            throw e;
+        }
     }
 
     @Test
-    public void testQueryNoResults() throws NamaraException {
-        Client client = new Client(NAMARA_HOST, API_KEY);
-        String queryString = "SELECT * FROM " + DATA_SET_ID + "/" + DATA_SET_VERSION + " LIMIT 0 OFFSET 0";
-        JSONObject response = client.query(queryString);
-        assertEquals(0, response.getJSONArray("results").length());
+    public void testQueryNoResults() throws NamaraException, IOException {
+        MockWebServer server = new MockWebServer();
+        server.enqueue(new MockResponse().setResponseCode(200).setBody("{\"results\":[]}"));
+        try {
+            HttpUrl baseUrl = server.url(Client.Endpoints.QUERY_ENDPOINT);
+
+            Client client = new Client(NAMARA_HOST, API_KEY);
+            String queryString = "SELECT * FROM " + DATA_SET_ID + "/" + DATA_SET_VERSION + " LIMIT 0 OFFSET 0";
+
+            JSONObject response = client.query(baseUrl, queryString);
+            assertEquals(0, response.getJSONArray("results").length());
+        } catch(NamaraException e) {
+            server.shutdown();
+            throw e;
+        }
     }
 }
--- src/test/java/namara/client/ResultSetTest.java
+++ src/test/java/namara/client/ResultSetTest.java
@@ -4,41 +4,63 @@ import namara.client.exception.NamaraException;
 import namara.client.exception.QueryException;
 import namara.query.Identifier;
 import namara.query.QueryBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 public class ResultSetTest {
-    /*
-     * NOTE: update this with necessary API keys and URLs
-     */
-    String API_KEY = "67902e8fe69efa4d82348de8bf5df077552cc66221f15c7593fb381113f69147";
-    String NAMARA_HOST = "https://namara-api-chris.ngrok.io/";
     String DATA_SET_ID = "e276a9a8-d06f-49a0-bd69-482920006b53";
     String DATA_SET_VERSION = "en-0";
 
     Identifier identifier = new Identifier(DATA_SET_ID, DATA_SET_VERSION);
     Identifier falseIdentifier = new Identifier("data-set-uuid", "en-0");
-    Client client = new Client(NAMARA_HOST, API_KEY);
 
     @Test
-    public void testResultIterator() {
-        QueryBuilder builder = new QueryBuilder(5).select().all().from().dataSet(identifier).getBuilder();
+    public void testResultIterator() throws NamaraException {
+        // Set up responses
+        JSONArray resultsCollection = new JSONArray()
+                .put(new JSONObject().put("c0", "xx").put("c1", "yy"))
+                .put(new JSONObject().put("c0", "zz").put("c1", "tt"))
+                .put(new JSONObject().put("c0", "hh").put("c1", "aa"));
+        JSONObject responseObject = new JSONObject().put("results", resultsCollection);
+        JSONObject emptyResponse = new JSONObject().put("results", new JSONArray());
+
+        // Set up mocks
+        Client client = mock(Client.class);
+        when(client.query(anyString())).thenReturn(responseObject).thenReturn(emptyResponse);
+
+        QueryBuilder builder = new QueryBuilder()
+                .select().all()
+                .from().dataSet(identifier)
+                .getBuilder();
+
         ResultSet resultSet = new ResultSet(builder, client);
         List<Record> recordList = new ArrayList();
         while(resultSet.hasNext()) {
             recordList.add(resultSet.next());
         }
-
-        assertEquals(5, recordList.size());
+        assertEquals(3, recordList.size());
     }
 
     @Test(expected = QueryException.class)
     public void testResultIteratorWithError() throws NamaraException {
-        QueryBuilder builder = new QueryBuilder(5).select().all().from().dataSet(falseIdentifier).getBuilder();
+        // Set up mocks
+        Client client = mock(Client.class);
+        when(client.query(anyString())).thenThrow(new QueryException("You did a bad query"));
+
+        QueryBuilder builder = new QueryBuilder(5)
+                .select().all()
+                .from().dataSet(falseIdentifier)
+                .getBuilder();
+
         ResultSet resultSet = new ResultSet(builder, client);
         List<Record> recordList = new ArrayList();
 
@@ -52,9 +74,21 @@ public class ResultSetTest {
     }
 
     @Test
-    public void testResultIteratorNoResults() {
-        QueryBuilder builder = new QueryBuilder(0).select().all().from().dataSet(identifier).getBuilder();
+    public void testResultIteratorNoResults() throws NamaraException {
+        // Set up responses
+        JSONObject emptyResponse = new JSONObject().put("results", new JSONArray());
+
+        // Set up mocks
+        Client client = mock(Client.class);
+        when(client.query(anyString())).thenReturn(emptyResponse);
+
+        QueryBuilder builder = new QueryBuilder(0)
+                .select().all()
+                .from().dataSet(identifier)
+                .getBuilder();
+
         ResultSet resultSet = new ResultSet(builder, client);
+
         List<Record> recordList = new ArrayList();
 
         while(resultSet.hasNext()) {
